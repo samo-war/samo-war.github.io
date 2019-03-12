ed07e8ebc70e1036bd67619257e3d5b6f277a9f2
samo-war <samo_war@mail.ru>
2019-03-13 01:12:53 +0300

    feat: add mentor-dashboard structure

---
+++ mentor-dashboard/precache-manifest.aeed208f1899c7e610025a4e9d616280.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "eae8255df19797948a04",
+    "url": "/static/js/main.eae8255d.chunk.js"
+  },
+  {
+    "revision": "56d478c575bbc6897b34",
+    "url": "/static/js/2.56d478c5.chunk.js"
+  },
+  {
+    "revision": "eae8255df19797948a04",
+    "url": "/static/css/main.d75d63b1.chunk.css"
+  },
+  {
+    "revision": "210502987754f33d6e4adf361f969103",
+    "url": "/index.html"
+  }
+];
